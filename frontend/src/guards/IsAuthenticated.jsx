a00dda618a43c54d9b73eead5cc583b72009efd3
BlackDragon36 <dimitrovtj@gmail.com>
2023-12-23 22:26:21 +0200

    Boilerplate code

--- frontend/src/guards/IsAuthenticated.jsx
+++ frontend/src/guards/IsAuthenticated.jsx
@@ -3,6 +3,7 @@ import { userStore } from "../state";
 
 const IsAuthenticated = () => {
   const isLoggedIn = userStore((state) => state.isLoggedIn);
+  console.log(isLoggedIn);
 
   if (!isLoggedIn) {
     return <Navigate to="/register" replace />;
